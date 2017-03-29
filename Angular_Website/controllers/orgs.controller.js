6697c1bc1cb7363993743b3443a2fe0c70563f5c
jlopez29 <jessealexanderlopez222@gmail.com>
2017-03-29 16:03:21 -0400

    Added angular framework

---
+++ Angular_Website/controllers/orgs.controller.js
@@ -0,0 +1,16 @@
+﻿var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('config.json');
+
+router.get('/orgs', function (req, res) {
+
+    res.render('orgs', viewData);
+});
+
+// router.post('/events', function (req, res) {
+//     // authenticate using api to maintain clean separation between layers
+//     });
+// });
+
+module.exports = router;
